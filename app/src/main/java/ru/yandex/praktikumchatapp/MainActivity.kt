5e7c0052da182ca9bf84bdeb250c80faeb71a28c
foxaice-ya-practicum <foxaice.swat@yandex.ru>
2025-11-01 01:51:46 +0700

    fix screen with keyboard display

--- app/src/main/java/ru/yandex/praktikumchatapp/MainActivity.kt
+++ app/src/main/java/ru/yandex/praktikumchatapp/MainActivity.kt
@@ -9,8 +9,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -50,6 +53,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PraktikumChatAppTheme {
                 Scaffold(
+                    contentWindowInsets = WindowInsets(0),
                     modifier = Modifier.fillMaxSize(),
                     topBar = {
                         TopAppBar(
@@ -60,7 +64,10 @@ class MainActivity : ComponentActivity() {
                     },
                     content = { innerPadding ->
                         ChatScreen(
-                            modifier = Modifier.padding(innerPadding)
+                            modifier = Modifier
+                                .padding(innerPadding)
+                                .imePadding()
+                                .navigationBarsPadding()
                         )
                     }
                 )
